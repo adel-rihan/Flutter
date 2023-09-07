1b7fb498f2efffffd7b85f651308b7482ae57f6a
Adel Rihan <adel.rihan@yahoo.com>
2023-09-07 18:27:57 +0300

    Adel Rihan

--- lib/layouts/home_layout.dart
+++ lib/layouts/home_layout.dart
@@ -17,38 +17,35 @@ class HomeLayout extends StatelessWidget {
         builder: (context, state) {
           HomeLayoutCubit cubit = HomeLayoutCubit.get(context);
 
-          return WillPopScope(
-            onWillPop: () async => false,
-            child: Scaffold(
-              key: cubit.scaffoldKey,
-              appBar: AppBar(
-                title: Text(
-                  cubit.titles[cubit.currentIndex],
+          return Scaffold(
+            key: cubit.scaffoldKey,
+            appBar: AppBar(
+              title: Text(
+                cubit.titles[cubit.currentIndex],
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    cubit.search(context);
+                  },
                 ),
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      cubit.search(context);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(LineAwesomeIcons.moon),
-                    onPressed: cubit.darkModeChange,
-                  ),
-                ],
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                onTap: (value) => cubit.changeIndex(context, value),
-                currentIndex: cubit.currentIndex,
-                items: [
-                  bottomNavItem(label: 'Business', icon: Icons.business),
-                  bottomNavItem(label: 'Sports', icon: Icons.sports),
-                  bottomNavItem(label: 'Science', icon: Icons.science),
-                ],
-              ),
-              body: SafeArea(child: cubit.currentScreen()),
+                IconButton(
+                  icon: const Icon(LineAwesomeIcons.moon),
+                  onPressed: cubit.darkModeChange,
+                ),
+              ],
             ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (value) => cubit.changeIndex(context, value),
+              currentIndex: cubit.currentIndex,
+              items: [
+                bottomNavItem(label: 'Business', icon: Icons.business),
+                bottomNavItem(label: 'Sports', icon: Icons.sports),
+                bottomNavItem(label: 'Science', icon: Icons.science),
+              ],
+            ),
+            body: SafeArea(child: cubit.currentScreen()),
           );
         },
       ),
